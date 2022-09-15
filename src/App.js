fbc88fd32ff87e3ff35914bffdfe8b76ba2b8367
ruk91 <sajithkasn@gmail.com>
2022-09-15 22:22:57 +0530

    Add age predicting functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('')
+  const [predictedData, setPredictedData] = useState(null)
+
+  const predictAge = () => {
+    fetch(`https://api.agify.io/?name=${name}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log('data: ', data)
+      setPredictedData(data)
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        placeholder='Enter your name here..'
+        onChange={(event) => setName(event.target.value)}
+      />
+
+      <button
+        onClick={predictAge}
+      >
+        Predict Age
+      </button>
+
+      <h2>Name: {predictedData?.name}</h2>
+      <h2>Age: {predictedData?.age}</h2>
+      <h2>Count: {predictedData?.count}</h2>
     </div>
   );
 }
